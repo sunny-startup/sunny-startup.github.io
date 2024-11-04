3219916f1d14ddd09543dd87f863b520826c0a46
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2024-11-04 22:20:54 +0800

    initial distribution

---
+++ src/components/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive
+      class="align-centerfill-height mx-auto"
+      max-width="900"
+    >
+      
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+  //
+</script>
